c417aa7fa02a589f343f6364a55bdada928cda5b
edgarango <edgarangog@gmail.com>
2025-10-12 15:20:37 -0500

    se completa funcion load_data para cargar archivo .csv

--- app.py
+++ app.py
@@ -21,9 +21,16 @@ app.config.suppress_callback_exceptions = True
 
 # Load data from csv
 def load_data():
-    # To do: Completar la función 
-    
+    #Cargar el archivo CSV
+    df = pd.read_csv('datos_energia.csv')
+    # Convertir la columna de fechas
+    df['time'] = pd.to_datetime(df['time'])
+    # Establecer la columna 'time' como el índice del DataFrame
+    df.set_index('time', inplace=True)
+    # Retornar el DataFrame procesado
+    return df
 
+    
 # Cargar datos
 data = load_data()
 
