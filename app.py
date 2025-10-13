07cf2d54a3d9cfe303eb0ea21d23dbdbd019a8cf
AErraez <arielon88@gmail.com>
2025-10-12 19:12:15 -0500

    Comentario Ariel

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.title = "Dashboard energia"
 server = app.server
 app.config.suppress_callback_exceptions = True
 
-
+#comentario ariel
 # Load data from csv
 def load_data():
     #Cargar el archivo CSV
